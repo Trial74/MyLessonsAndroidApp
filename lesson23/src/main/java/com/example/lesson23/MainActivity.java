56c39e8f6c2ba43e28540d5dea12879cf47a8251
Trial <51761394+trial74@users.noreply.github.com>
2023-10-23 12:43:53 +0500

    Состояния Activity
    Signed-off-by: Extreme-Look <extremelookapp@gmail.com>

--- lesson23/src/main/java/com/example/lesson23/MainActivity.java
+++ lesson23/src/main/java/com/example/lesson23/MainActivity.java
@@ -12,6 +12,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d(TAG, "MainActivity: onCreate()");
     }
 
     @Override
